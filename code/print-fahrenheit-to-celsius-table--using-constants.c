6fb3e4fa6d1340ff864a2e2c85ed960c0cd72a40
Roy Vanegas <roy@thecodeeducators.com>
2025-01-29 19:26:07 -0500

    Add print-fahrenheit-to-celsius-table--using-constants.c

---
+++ code/print-fahrenheit-to-celsius-table--using-constants.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+#define LOWER 0
+#define UPPER 300
+#define STEP  20
+
+int main() {
+  int fahrenheit, celsius;
+
+  fahrenheit = LOWER;
+
+  printf("F\tC\n");
+  printf("-----------\n");
+
+  while (fahrenheit <= UPPER) {
+    celsius = 5 * (fahrenheit - 32) / 9;
+
+    printf("%d\t%d\n", fahrenheit, celsius);
+
+    fahrenheit += STEP;
+  }
+
+  return 0;
+}
