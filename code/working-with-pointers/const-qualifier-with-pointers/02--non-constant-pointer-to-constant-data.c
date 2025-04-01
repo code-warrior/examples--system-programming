7a37146d44f91d5d51c2621a05ef24f349cf5e26
Roy Vanegas <roy@thecodeeducators.com>
2025-04-01 09:01:09 -0400

    Add 02--non-constant-pointer-to-constant-data.c

---
+++ code/working-with-pointers/const-qualifier-with-pointers/02--non-constant-pointer-to-constant-data.c
@@ -0,0 +1,28 @@
+// fig07_07.c
+// Printing a string one character at a time using
+// a non-constant pointer to constant data.
+
+// #include <ctype.h> // Required for “toupper”
+#include <stdio.h>
+#include "../../libs/colors.h"
+
+void printCharacters(char *sPtr);
+
+int main(void) {
+  char string[] = "Characters in a string";
+
+  puts("The string is:");
+  puts(BLUE);
+  printCharacters(string);
+  puts(NORMAL);
+}
+
+// sPtr cannot be used to modify the character to which it points,
+// i.e., sPtr is a “read-only” pointer
+// Read right to left as, “sPtr is a pointer to a character constant”
+void printCharacters(char *sPtr) {
+  for (; *sPtr != '\0'; ++sPtr) {
+    printf("%c", *sPtr);
+    // *sPtr = toupper(*sPtr); // Throws error, since sPtr is read-only
+  }
+}
