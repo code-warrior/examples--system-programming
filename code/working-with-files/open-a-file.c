e4c94b342375f1c729b20bd14462e184ed54129a
Roy Vanegas <roy@thecodeeducators.com>
2025-04-09 17:33:46 -0400

    Add open-a-file.c

---
+++ code/working-with-files/open-a-file.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main (void) {
+  FILE * file;
+  char *filename = "open-a-file.c";
+
+  printf("The maximum length of a filename on your machine is %d bytes\n",
+    FILENAME_MAX);
+  printf("Attempting to open file `%s` in read only mode...\n",
+    filename);
+
+  puts("");
+
+  if ((file = fopen(filename, "r")) != NULL) {
+    int character;
+
+    while ((character = fgetc(file)) != EOF) {
+      printf("%c", character);
+    }
+  } else {
+    printf( "\n\tFile error or file does not exist.\n\tExiting... \n\n" );
+
+    return -1;
+  }
+
+  return 0;
+}
